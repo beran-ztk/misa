daaad4ac7de86ddfb7ea6b9b04f3f7c00ca04172
Beran Öztürk <beran-ozturk2002@outlook.de>
2025-11-30 16:19:19 +0100

    Init Commit

---
+++ Misa.Ui.Avalonia/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Misa.Ui.Avalonia.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Greeting { get; } = "Welcome to Avalonia!";
+}
---
+++ Misa.Ui.Avalonia/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Misa.Ui.Avalonia.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
