8bfb9612eca7ce667bdeb09b0da2481171246d0c
Beran Öztürk <beran-ozturk2002@outlook.de>
2025-12-27 18:32:23 +0100

    Deadline Endpoint and simple Api refactoring

---
+++ Misa.Api/Endpoints/Scheduling/DeadlineEndpoints.cs
@@ -0,0 +1,21 @@
+﻿using Misa.Application.Scheduling.Commands.SetEntityDeadline;
+using Misa.Contract.Scheduling;
+
+namespace Misa.Api.Endpoints.Scheduling;
+
+public static class DeadlineEndpoints
+{
+    public static void Map(WebApplication app)
+    {
+        app.MapPut("/entities/deadline", SetDeadline);
+    }
+
+    private static async Task<IResult> SetDeadline(ScheduleDto scheduleDto, SetEntityDeadlineHandler handler)
+    {
+        await handler.Handle(
+            new SetEntityDeadlineCommand(scheduleDto.EntityId, scheduleDto.StartAtUtc)
+        );
+        
+        return Results.NoContent();
+    }
+}
