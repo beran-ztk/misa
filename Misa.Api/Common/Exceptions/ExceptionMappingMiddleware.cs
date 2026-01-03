13df8d619812826170dc22402a3a9158940c70a0
Beran Öztürk <beran-ozturk2002@outlook.de>
2026-01-03 18:50:39 +0100

    ValidationException in ExceptionMiddleware

--- Misa.Api/Common/Exceptions/ExceptionMappingMiddleware.cs
+++ Misa.Api/Common/Exceptions/ExceptionMappingMiddleware.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
 using Microsoft.AspNetCore.Mvc;
 using Misa.Application.Common.Exceptions;
 
@@ -21,6 +22,21 @@ public sealed class ExceptionMappingMiddleware : IMiddleware
             {
                 Status = StatusCodes.Status404NotFound,
                 Title = "Not found",
+                Detail = ex.Message,
+                Instance = context.Request.Path
+            };
+
+            await context.Response.WriteAsync(JsonSerializer.Serialize(problem));
+        }
+        catch (ValidationException ex)
+        {
+            context.Response.StatusCode = StatusCodes.Status400BadRequest;
+            context.Response.ContentType = "application/problem+json";
+
+            var problem = new ProblemDetails
+            {
+                Status = StatusCodes.Status400BadRequest,
+                Title = "Bad Request",
                 Detail = ex.Message,
                 Instance = context.Request.Path
             };
