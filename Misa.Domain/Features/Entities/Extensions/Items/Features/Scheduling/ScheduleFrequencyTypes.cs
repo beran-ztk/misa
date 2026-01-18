ca1eafdd330af5538ce2343e3c71c886fe663a4c
Beran Öztürk <beran-ozturk2002@outlook.de>
2026-01-18 15:12:52 +0100

    add lookup types as enum

---
+++ Misa.Domain/Features/Entities/Extensions/Items/Features/Scheduling/ScheduleFrequencyTypes.cs
@@ -0,0 +1,15 @@
+﻿namespace Misa.Domain.Features.Entities.Extensions.Items.Features.Scheduling;
+
+/// <summary>
+/// Defines the base frequency type for a scheduler.
+/// </summary>
+public enum ScheduleFrequencyTypes
+{
+    Once = 1,
+    Minutes = 2,
+    Hours = 3,
+    Days = 4,
+    Weeks = 5,
+    Months = 6,
+    Years = 7
+}
