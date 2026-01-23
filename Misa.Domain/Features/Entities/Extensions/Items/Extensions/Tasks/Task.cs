a3f4df4b5876d2bbce650b7fecf735d9f3275532
beran <beran-ozturk2002@outlook.de>
2026-01-23 23:17:41 +0100

    Task Domain Modell

---
+++ Misa.Domain/Features/Entities/Extensions/Items/Extensions/Tasks/Task.cs
@@ -0,0 +1,14 @@
+﻿namespace Misa.Domain.Features.Entities.Extensions.Items.Extensions.Tasks;
+
+public class Task
+{
+    private Task() {}
+
+    public Task(TaskCategory category)
+    {
+        Category = category;
+    }
+    
+    public Guid Id { get; private set; }
+    public TaskCategory Category { get; private set; }
+}
