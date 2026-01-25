189371ec0fcd0f2328632a274344e52dafa7ceca
beran <beran-ozturk2002@outlook.de>
2026-01-25 12:14:08 +0100

    delete unused classes

--- Misa.Ui.Avalonia/App/Shell/InfoBarViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿using Misa.Ui.Avalonia.Infrastructure.Services.Navigation;
-using Misa.Ui.Avalonia.Presentation.Mapping;
-
-namespace Misa.Ui.Avalonia.App.Shell;
-
-public partial class InfoBarViewModel(INavigationService navigationService) : ViewModelBase
-{
-    private readonly INavigationService _navigationService = navigationService;
-}
--- Misa.Ui.Avalonia/App/Shell/StatusBarViewModel.cs
+++
@@ -1,6 +0,0 @@
-﻿using Misa.Ui.Avalonia.Infrastructure.Services.Navigation;
-using Misa.Ui.Avalonia.Presentation.Mapping;
-
-namespace Misa.Ui.Avalonia.App.Shell;
-
-public class StatusBarViewModel : ViewModelBase;
