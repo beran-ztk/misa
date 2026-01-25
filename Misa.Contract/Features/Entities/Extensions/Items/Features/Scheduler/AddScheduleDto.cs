9c3a813b74b49ae5b694887de6bdc437696cab8c
beran <beran-ozturk2002@outlook.de>
2026-01-25 12:13:48 +0100

    Frequency Mapping

---
+++ Misa.Contract/Features/Entities/Extensions/Items/Features/Scheduler/AddScheduleDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Misa.Contract.Features.Entities.Extensions.Items.Features.Scheduler;
+
+public sealed class AddScheduleDto
+{
+    public required ScheduleFrequencyTypeContract ScheduleFrequencyType { get; init; }
+    public required int FrequencyInterval { get; init; }
+}
