4edc4dbc184acd5a890f7b2b4bb0f5886e6eae0e
beran <beran-ozturk2002@outlook.de>
2026-01-27 23:04:41 +0100

    .

--- Misa.Application/Features/Entities/Extensions/Items/Features/Scheduling/Commands/SchedulePlanningHandler.cs
+++ Misa.Application/Features/Entities/Extensions/Items/Features/Scheduling/Commands/SchedulePlanningHandler.cs
@@ -8,13 +8,27 @@ public class SchedulePlanningHandler(ISchedulerPlanningRepository repository)
     public async Task HandleAsync(SchedulePlanningCommand command, CancellationToken stoppingToken)
     {
         var now = DateTimeOffset.UtcNow;
-        var furthestLookaheadTimeAllowed = now.AddYears(1);
         
         var schedules = await repository.GetActiveSchedulesAsync(now, stoppingToken);
 
         foreach (var schedule in schedules)
         {
+            if (schedule.ScheduleFrequencyType != ScheduleFrequencyType.Minutes
+                && schedule.ScheduleFrequencyType != ScheduleFrequencyType.Hours
+                && schedule.ScheduleFrequencyType != ScheduleFrequencyType.Days)
+            {
+                break;
+            }
+            
             var currentLookaheadCount = await  repository.GetExecutionCountPlannedAheadAsync(schedule.Id, now, stoppingToken);
+            
+            var furthestLookaheadTimeAllowed = schedule.ScheduleFrequencyType switch
+            {
+                ScheduleFrequencyType.Minutes => now.AddDays(1),
+                ScheduleFrequencyType.Hours => now.AddDays(30),
+                ScheduleFrequencyType.Days => now.AddDays(180),
+                _ => now
+            };
             
             while (schedule.OccurrenceCountLimit != 0 
                    && currentLookaheadCount < schedule.LookaheadLimit
@@ -24,20 +38,30 @@ public class SchedulePlanningHandler(ISchedulerPlanningRepository repository)
                 schedule.ReduceOccurrenceCount();
                 schedule.CheckAndUpdateNextAllowedExecution(now);
                 
-                switch (schedule.ScheduleFrequencyType) 
-                { 
-                    case ScheduleFrequencyType.Minutes:
-                        schedule.NextDueAtUtc = schedule.SchedulingAnchorUtc + TimeSpan.FromMinutes(schedule.FrequencyInterval);
-                        break;
-                    case ScheduleFrequencyType.Hours:
-                        schedule.NextDueAtUtc = schedule.SchedulingAnchorUtc + TimeSpan.FromHours(schedule.FrequencyInterval);
-                        break;
-                    default:
-                        throw new ArgumentOutOfRangeException(
-                            nameof(ScheduleFrequencyType), 
-                            schedule.ScheduleFrequencyType.ToString(), 
-                            null);
-                }
+                var delta = schedule.ScheduleFrequencyType switch
+                {
+                    ScheduleFrequencyType.Minutes => TimeSpan.FromMinutes(schedule.FrequencyInterval),
+                    ScheduleFrequencyType.Hours => TimeSpan.FromHours(schedule.FrequencyInterval),
+                    ScheduleFrequencyType.Days => TimeSpan.FromDays(schedule.FrequencyInterval),
+                    
+                    _ => throw new ArgumentOutOfRangeException(
+                        nameof(ScheduleFrequencyType),
+                        schedule.ScheduleFrequencyType.ToString(),
+                        null)
+                };
+
+                do
+                {
+                    schedule.NextDueAtUtc = schedule.SchedulingAnchorUtc.Add(delta);
+                } 
+                while 
+                (
+                    (schedule.StartTime.HasValue && TimeOnly.FromTimeSpan(schedule.SchedulingAnchorUtc.TimeOfDay) <
+                        schedule.StartTime)
+                    &&
+                    (schedule.EndTime.HasValue && TimeOnly.FromTimeSpan(schedule.SchedulingAnchorUtc.TimeOfDay) >
+                        schedule.EndTime)
+                );
 
                 if (schedule.SchedulingAnchorUtc > now)
                 {
