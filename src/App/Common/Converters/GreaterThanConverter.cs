fafa39135e271aa337c72743d12144e5fc647a5c
beran <beran-ozturk2002@outlook.de>
2026-02-22 15:15:29 +0100

    card view at smaller width

---
+++ src/App/Common/Converters/GreaterThanConverter.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+
+namespace Misa.Ui.Avalonia.Common.Converters;
+
+public class GreaterThanConverter : IValueConverter
+{
+    public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        if (value is double width &&
+            parameter is string param &&
+            double.TryParse(param, out var threshold))
+        {
+            return width > threshold;
+        }
+
+        return false;
+    }
+
+    public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
