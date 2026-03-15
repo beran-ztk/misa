8be808f603f03a59dc334d03ca07dd42d947acae
beran <beran-ozturk2002@outlook.de>
2026-03-15 11:51:52 +0100

    inspector entry layout; edit outsources

---
+++ src/App/Features/Inspector/Tabs/Entry/Base/InspectorEntryBodyView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Misa.Ui.Avalonia.Features.Inspector.Tabs.Entry.Base;
+
+public partial class InspectorEntryBodyView : UserControl
+{
+    public InspectorEntryBodyView()
+    {
+        InitializeComponent();
+    }
+}
